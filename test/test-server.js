b38b441d5e6ccc5aa0419992667488005d54a804
ssheaks <sarah.sheaks@gmail.com>
2018-03-09 11:46:46 -0800

    new branch for testing

--- test/test-server.js
+++ test/test-server.js
@@ -25,7 +25,7 @@ describe('Recipes', function() {
     return closeServer();
   });
 
-  it.only('should list items on GET', function() {
+  it('should list items on GET', function() {
     return chai.request(app)
       .get('/recipes')
       .then(function(res) {
@@ -43,7 +43,7 @@ describe('Recipes', function() {
       });
   });
 
-  it.only('should add an item on POST', function() {
+  it('should add an item on POST', function() {
     const newItem = {name: 'smoothie', ingredients: ['yogurt', 'fruit', 'juice']};
     return chai.request(app)
       .post('/recipes')
@@ -58,44 +58,29 @@ describe('Recipes', function() {
       });
   });
 
-  // test strategy:
-  //  1. initialize some update data (we won't have an `id` yet)
-  //  2. make a GET request so we can get an item to update
-  //  3. add the `id` to `updateData`
-  //  4. Make a PUT request with `updateData`
-  //  5. Inspect the response object to ensure it
-  //  has right status code and that we get back an updated
-  //  item with the right data in it.
+  
   it('should update items on PUT', function() {
-    // we initialize our updateData here and then after the initial
-    // request to the app, we update it with an `id` property so
-    // we can make a second, PUT call to the app.
+
     const updateData = {
       name: 'foo',
-      checked: true
+      ingredients: ['bar', 'fizz']
     };
 
     return chai.request(app)
       // first have to get so we have an idea of object to update
-      .get('/shopping-list')
+      .get('/recipes')
       .then(function(res) {
         updateData.id = res.body[0].id;
-        // this will return a promise whose value will be the response
-        // object, which we can inspect in the next `then` block. Note
-        // that we could have used a nested callback here instead of
-        // returning a promise and chaining with `then`, but we find
-        // this approach cleaner and easier to read and reason about.
+       
         return chai.request(app)
-          .put(`/shopping-list/${updateData.id}`)
+          .put(`/recipes/${updateData.id}`)
           .send(updateData);
       })
-      // prove that the PUT request has right status code
-      // and returns updated item
       .then(function(res) {
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a('object');
-        expect(res.body).to.deep.equal(updateData);
+        expect(res).to.have.status(204);
+        //expect(res).to.be.json;
+        //expect(res.body).to.be.a('object');
+        //expect(res.body).to.deep.equal(updateData);
       });
   });
 
@@ -107,10 +92,10 @@ describe('Recipes', function() {
     return chai.request(app)
       // first have to get so we have an `id` of item
       // to delete
-      .get('/shopping-list')
+      .get('/recipes')
       .then(function(res) {
         return chai.request(app)
-          .delete(`/shopping-list/${res.body[0].id}`);
+          .delete(`/recipes/${res.body[0].id}`);
       })
       .then(function(res) {
         expect(res).to.have.status(204);
